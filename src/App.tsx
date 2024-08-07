88c2cd6b83f5784ce7312db79ba3d8a0cd6f92e9
Michaela Bereza <michaelabereza@icloud.com>
2024-08-08 00:12:35 +0300

    card and deck types and creating the deck

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+
+type Suit = "Hearts" | "Diamonds" | "Clubs" | "Spades"
+type Rank =
+  | "2"
+  | "3"
+  | "4"
+  | "5"
+  | "6"
+  | "7"
+  | "8"
+  | "9"
+  | "10"
+  | "J"
+  | "Q"
+  | "K"
+  | "A"
+
+interface Card {
+  rank: Rank
+  suit: Suit
+}
+
+const createDeck = (): Card[] => {
+  const suits: Suit[] = ["Hearts", "Diamonds", "Clubs", "Spades"]
+  const ranks: Rank[] = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ]
+
+  let deck: Card[] = []
+
+  for (let suit of suits) {
+    for (let rank of ranks) {
+      deck.push({rank, suit})
+    }
+  }
+  // console.log(deck)
+  return deck
+}
+
+// createDeck()
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>let's get coding</h1>
     </>
   )
 }
