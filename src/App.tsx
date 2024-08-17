413b066d75d712eeb1f8221df243d97a9514e273
Michaela Bereza <michaelabereza@icloud.com>
2024-08-17 12:57:18 +0300

    check for straight

--- src/App.tsx
+++ src/App.tsx
@@ -53,9 +53,7 @@ const createDeck = (): Card[] => {
 // createDeck()
 
 const isFlush = (cards: Card[]): boolean => {
-  // Map the cards array to get an array of suits
   const suits = cards.map((card) => card.suit)
-  // Check if all suits are the same by comparing each suit to the first suit
   return suits.every((suit) => suit === suits[0])
 }
 
@@ -77,6 +75,61 @@ const isFlush = (cards: Card[]): boolean => {
 
 // console.log(isFlush(hand1)) // Output: true
 // console.log(isFlush(hand2)) // Output: false
+
+const rankOrder: Rank[] = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+]
+
+const sortByRank = (cards: Card[]): Card[] => {
+  return cards.sort(
+    (a, b) => rankOrder.indexOf(a.rank) - rankOrder.indexOf(b.rank)
+  )
+}
+
+const isStraight = (cards: Card[]): boolean => {
+  const sortedCards = sortByRank(cards)
+
+  const rankIndices = sortedCards.map((card) => rankOrder.indexOf(card.rank))
+
+  for (let i = 0; i < rankIndices.length - 1; i++) {
+    if (rankIndices[i] + 1 !== rankIndices[i + 1]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+// const hand1: Card[] = [s
+//   {rank: "6", suit: "Hearts"},
+//   {rank: "2", suit: "Diamonds"},
+//   {rank: "3", suit: "Hearts"},
+//   {rank: "4", suit: "Diamonds"},
+//   {rank: "5", suit: "Hearts"}
+// ]
+
+// const hand2: Card[] = [
+//   {rank: "7", suit: "Hearts"},
+//   {rank: "K", suit: "Diamonds"},
+//   {rank: "9", suit: "Diamonds"},
+//   {rank: "10", suit: "Diamonds"},
+//   {rank: "J", suit: "Hearts"}
+// ]
+
+// console.log(isStraight(hand1)) // Output: true
+// console.log(isStraight(hand2)) // Output: false
 
 function App() {
   return (
